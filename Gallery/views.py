6d09e3ced88cf8420200f410494d6ca925ac37c6
Loisa-Kitakaya <loisakitakaya@gmail.com>
2020-03-02 02:51:10 +0300

    created views for the application,s urls

--- Gallery/views.py
+++ Gallery/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import *
+from .models import *
 
 # Create your views here.
 
@@ -10,4 +12,31 @@ def welcome(request):
 ## index view
 def index(request):
     return render(request, "index.html")
+
+
+## image upload view
+def image_upload(request):
+
+    if request.method == "POST":
+
+        image_form = ImageUpload(request.POST, request.FILES)
+
+        if image_form.is_valid():
+
+            img = Images.objects.get(pk=id)
+            loc = Location.objects.get(pk=id)
+            cat = Category.objects.get(pk=id)
+
+            img.image = image_form.cleaned_data["image"]
+            img.image_name = image_form.cleaned_data["image_name"]
+            img.image_description = image_form.cleaned_data["image_description"]
+            img.save()
+
+            loc.location = image_form.cleaned_data["location"]
+            loc.save()
+
+            cat.category = image_form.cleaned_data["category"]
+            cat.save()
+
+    return redirect("index")
 
