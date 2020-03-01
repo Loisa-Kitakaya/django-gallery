450474a34c379588cbe777d2a6cca18bb89a3e1e
Loisa-Kitakaya <loisakitakaya@gmail.com>
2020-03-01 23:37:22 +0300

    created and connected app to database

--- DjangoGalleryProject/settings.py
+++ DjangoGalleryProject/settings.py
@@ -77,8 +77,10 @@ WSGI_APPLICATION = "DjangoGalleryProject.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "gallery",
+        "USER": "loisa",
+        "PASSWORD": "Kitloisa15",
     }
 }
 
