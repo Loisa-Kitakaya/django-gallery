25bb6db8b29991257a6d0cc3fc46850cb1c53c67
Loisa-Kitakaya <loisakitakaya@gmail.com>
2020-03-02 06:40:15 +0300

    changed some settings

--- DjangoGalleryProject/settings.py
+++ DjangoGalleryProject/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv["SECRET_KEY"]
+SECRET_KEY = "kp%d1ya+huwr!p5(ygwigl6rs%63w578d-wb1f#8g!*mz*3d_="
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv["DEBUG"]
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
